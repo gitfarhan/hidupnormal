1886ad6e20c0b65236ffcbe5fa695ff384d3f65c
farhan <farrrrrhan@gmail.com>
2018-12-23 14:46:07 +0700

    fixing

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from datetime import datetime, date
 import string
+import uuid
 import random
 import os
+from flask_restful import Resource, Api
+from urllib.parse import urlsplit
 
 app = Flask(__name__)
+api = Api(app)
 
-def generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
 
-def anothe_generator(length=8):
-    chars = string.ascii_letters + string.digits + '!@#$%^&*()'
-    random.seed = (os.urandom(1024))
-    return ''.join(random.choice(chars) for i in range(length))
+def another_generator(length=8):
+    return str(uuid.uuid1()).join(random.choices(string.ascii_uppercase + string.digits, k=5)).replace("-","")[:length]
 
 @app.route('/')
 def index():
@@ -39,9 +39,7 @@ def insert():
         month = tanggal[5:7]
         day = tanggal[8:10]
 
-        password = anothe_generator(length=10)
-        gm = generator(size=8)
-        email = '{m}@hidupnormal.net'.format(m=gm)
+        password = another_generator(length=10)
         code = '{d}{m}{y}{p}'.format(d=day, m=month, y=year, p=password)
 
         now = datetime.now()
@@ -62,7 +60,6 @@ def insert():
             'date' : fin,
             'total' : total,
             'password' : password,
-            'email' : email,
             'code' : code,
             'enc' : enc
         })
@@ -87,7 +84,7 @@ def process():
     if kode:
         dec = decrypt(kode)
 
-        # memsiahkan hasil
+        # memisahkan hasil
 
         day = dec[0:2]
         month = dec[2:4]
@@ -125,6 +122,75 @@ def process():
             "error" : "missing data"
         })
 
+# hidup normal API
+
+class Encode(Resource):
+    def get(self, code):
+        url_data = urlsplit(code)
+        dec = encrypt(url_data)
+
+        # memsiahkan hasil
+        day = dec[0:2]
+        month = dec[2:4]
+        year = dec[4:8]
+        password = dec[8:]
+
+        # menghitung sisa hari
+        now = datetime.now()
+        nowYear = now.year
+        nowMonth = now.month
+        nowDay = now.day
+
+        d0 = date(int(year), int(month), int(day))
+        d1 = date(nowYear, nowMonth, nowDay)
+        delta = d0 - d1
+        total = delta.days
+
+        if total <= 0:
+            return {'password' : password}
+        else:
+            return {'total' : total}
+
+class GetPassword(Resource):
+    def get(self, year, month, day):
+        password = another_generator(length=10)
+        code = '{d}{m}{y}{p}'.format(d=day, m=month, y=year, p=password)
+
+        now = datetime.now()
+        nowYear = now.year
+        nowMonth = now.month
+        nowDay = now.day
+
+        d0 = date(int(year), int(month), int(day))
+        d1 = date(nowYear, nowMonth, nowDay)
+        delta = d0 - d1
+        total = (delta.days)
+
+        enc = encrypt(code)
+
+        fin = "{day}/{month}/{year}".format(day=day, month=month, year=year)
+
+        result = {
+            'date': fin,
+            'total': total,
+            'password': password,
+            'code': code,
+            'enc': enc
+        }
+
+        if total < 0:
+            return {'error' : 'haha?'}
+        else:
+            return result
+
+api.add_resource(GetPassword, '/get/<int:year>/<int:month>/<int:day>')
+api.add_resource(Encode, '/encode/<string:code>')
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port=int(os.environ.get("PORT", 5000)), host='0.0.0.0')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5000)), host='0.0.0.0')
+    app.run(debug=True, port=3000)
+    # password = another_generator(10)
+    # print(password)
+    # print(len(password))
